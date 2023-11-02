b4735e0cd3ff9bfe5efdf4ce6dc566fa92c7fe2a
rahilahmed95.com <rahilahmed8495@gmail.com>
2023-11-02 17:58:02 +0530

    updated

--- server/index.js
+++ server/index.js
@@ -36,9 +36,7 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(
-  cors({ origin: ["http://localhost:3001", "https://Salaam.onrender.com"] })
-);
+app.use(cors());
 // images locally store hongey
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
